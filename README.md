89e956cdc396cbf1d75213030197000ad0f1fdbf
Sportsguy <mahoward2s@gmail.com>
2020-12-11 10:34:51 -0600

    changed description in Read me, and testing things in JS

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
      You will be able to block out time and set aside periods of time for work, lunch, or other activites.
      That way you will not have to worry about missing another meeting or running late ever again.
 
+     Utilizing moment.js, JavaScript, and local storage I created a daily planner that will allow a user to track what they have planned for their work day.
+
 <!--Link To Active Site-->
 https://mahoward2s.github.io/My-Daily-Work-Planner/
 
--- script.js
+++ script.js
@@ -65,3 +65,58 @@ var timeSlots = [
     },
 ];
 
+//local storage to hold user input
+localStorage.setItem("userInput", "");
+document.getElementById("result").innerHTML = localStorage.getItem("userInput");
+
+
+timeSlots.forEach(function (thishour) {
+    var hourRow = $("<form>").attr({
+        "class": "row"
+    });
+    $(".container").append(hourRow);
+
+    var hourField = $("<div>")
+        .text('${thisHour.hour}${thisHour.meridian}')
+        .attr({
+            "class": "col-md-2 hour"
+        });
+
+    var hourPlan = $("<div>")
+        .attr({
+            "class": "col-md-9 description p-0"
+        });
+    var planData = $("<textarea>");
+    hourPlan.append(planData);
+    planData.attr("id", thisHour.id);
+    if (thisHour.time < moment().format("HH")) {
+        planData.attr({
+            "class": "past",
+        })
+    } else if (thisHour.time === moment().format("HH")) {
+        planData.attr({
+            "class": "present"
+        })
+    } else if (thisHour.time > moment().format("HH")) {
+        planData.attr({
+            "class": "future"
+        })
+    }
+    var saveButton = $("<i class='far fa-save fa-lg'></i>")
+    var savePlan = $("<button>")
+        .attr({
+            "class": "col-md-1 saveBtn"
+        });
+    savePlan.append(saveButton);
+    hourRow.append(hourField, hourPlan, savePlan);
+})
+init();
+
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+    var saveIndex = $(this).siblings(".description").children(".future").attr("id");
+    timeSlots[saveIndex].reminder = $(this).siblings(".description").children(".future").val();
+    console.log(saveIndex);
+    saveReminders();
+    displayReminders();
+})
